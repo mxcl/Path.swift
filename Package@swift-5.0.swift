d79844cf2b219f2689ebd13c3fbd28dadc83230a
Max Howell <mxcl@me.com>
2019-02-13 15:53:41 -0500

    Use a symlink to prevent Package.swift divergence

--- Package@swift-5.0.swift
+++ Package@swift-5.0.swift
@@ -1,17 +1,1 @@
-// swift-tools-version:5.0
-import PackageDescription
-
-let pkg = Package(
-    name: "Path.swift",
-    products: [
-        .library(name: "Path", targets: ["Path"]),
-    ],
-    targets: [
-        .target(name: "Path", path: "Sources"),
-        .testTarget(name: "PathTests", dependencies: ["Path"]),
-    ]
-)
-
-pkg.swiftLanguageVersions = [
-    .v4_2, .v5
-]
+Package.swift
