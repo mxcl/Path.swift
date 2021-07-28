6e8a42f01da96532a2d151450fe651eea71cd8c2
Rob Jonson <rob@hobbyistsoftware.com>
2021-07-28 15:06:20 +0100

    Fix assert error message

--- Tests/PathTests/etc.swift
+++ Tests/PathTests/etc.swift
@@ -19,7 +19,7 @@ private func logic<P: Pathish>(_ set1: Set<Path>, _ set2: Set<Path>, relativeTo:
     if set1 != set2 {
         let cvt: (Path) -> String = { $0.relative(to: relativeTo) }
         let out1 = set1.map(cvt).sorted()
-        let out2 = set1.map(cvt).sorted()
+        let out2 = set2.map(cvt).sorted()
         fail("Set(\(out1)) is not equal to Set(\(out2))")
     }
 }
