a7f535cc490e0b9d4f0aee2278c0b681417d49f7
removedporn <86824510+removedporn@users.noreply.github.com>
2021-09-02 01:43:17 +0800

    Delete Sources directory

--- Sources/Pathish.swift
+++
@@ -1,6 +0,0 @@
-/// A type that represents a filesystem path, if you conform your type
-/// to `Pathish` it is your responsibility to ensure the string is correctly normalized
-public protocol Pathish: Hashable, Comparable {
-    /// The normalized string representation of the underlying filesystem path
-    var string: String { get }
-}
